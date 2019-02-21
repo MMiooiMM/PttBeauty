b892505fdb3ad9b0e5f13950ce2d2ec844954de7
mio <mio093611@gmail.com>
2019-02-21 10:12:35 +0800

    爬文功能

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ admin.initializeApp({
 });
 
 var db = admin.firestore();
+const BeautyRef = db.collection('Beauty');
 
 // 引用linebot SDK
 const line = require('@line/bot-sdk');
@@ -42,8 +43,7 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-  const BeautyRef = db.collection('Beauty');
-  if (event.message.text == '.') {
+  if (event.message.text == '抽') {
     BeautyRef.get().then(snapshot => {
         var random = getRandomInt(snapshot.size)
         var index = 0;
@@ -74,52 +74,9 @@ function handleEvent(event) {
       .catch(err => {
         console.log('Error getting documents', err);
       });
-  } else if (event.message.text == '..') {
-    request('https://www.ptt.cc/bbs/Beauty/index.html', function (error, response, body) {
-      var begin = 0;
-      var end = 0;
-      var stop = body.indexOf('r-list-sep');
-      while (true) {
-        begin = body.indexOf('r-ent', end);
-        end = body.indexOf('meta', begin);
-        if (begin > stop)
-          break;
-        const nrec = (body.indexOf('span', begin) > end) ? 0 : body.substring(body.indexOf('<span', begin) + 20, body.indexOf('</span>', begin));
-        const title = body.substring(body.indexOf('html">', begin) + 6, body.indexOf('</a>', begin));
-        const url = body.substring(body.indexOf('href="', begin) + 6, body.indexOf('html">', begin) + 4);
-        BeautyRef.where('url', '==', url).get().then(snap => {
-          const size = snap.size; // will return the collection size          
-
-          if (size == 0) {
-            BeautyRef.add({
-              nrec: nrec,
-              title: title,
-              url: url
-            }).then((ref) => {
-              request(`https://www.ptt.cc${url}`, function (error, response, body) {
-                var _begin = 0;
-                var _end = 0;
-                while (true) {
-                  _begin = body.indexOf('nofollow', _end);
-                  _end = body.indexOf('</a>', _begin);
-                  if (_begin == -1)
-                    break;
-                  ref.collection('images').add({
-                    url: body.substring(_begin + 10, _end)
-                  });
-                }
-              });
-            });
-          } else {
-            snap.forEach(doc => {
-              BeautyRef.doc(doc.id).update({
-                nrec: nrec
-              })
-            })
-          }
-        });
-      }
-    });
+  } else if (event.message.text == '給我去爬文') {
+    var url = 'https://www.ptt.cc/bbs/Beauty/index.html';
+    getPage(url, 20);
   } else {
     return Promise.resolve(200);
   }
@@ -133,4 +90,68 @@ app.listen(port, () => {
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getPage(url, count) {
+  if (count <= 0)
+    return;
+  console.log(url);
+  new Promise(resolve => {
+    request(url, function (error, response, body) {
+      getBeauty(url);
+      var begin = body.indexOf('最舊') + 37;
+      var end = body.indexOf('上頁') - 11;
+      resolve(`https://www.ptt.cc${body.substring(begin, end)}`);
+    })
+  }).then(value => {
+    getPage(value, count--);
+  })
+}
+
+function getBeauty(src) {
+  request(src, function (error, response, body) {
+    var begin = 0;
+    var end = 0;
+    var stop = body.indexOf('r-list-sep');
+    while (true) {
+      begin = body.indexOf('r-ent', end);
+      end = body.indexOf('meta', begin);
+      if ((begin > stop && stop != -1) || begin == -1)
+        break;
+      const nrec = (body.indexOf('span', begin) > end) ? 0 : body.substring(body.indexOf('<span', begin) + 20, body.indexOf('</span>', begin));
+      const title = body.substring(body.indexOf('html">', begin) + 6, body.indexOf('</a>', begin));
+      const url = body.substring(body.indexOf('href="', begin) + 6, body.indexOf('html">', begin) + 4);
+      BeautyRef.where('url', '==', url).get().then(snap => {
+        const size = snap.size; // will return the collection size          
+
+        if (size == 0) {
+          BeautyRef.add({
+            nrec: nrec,
+            title: title,
+            url: url
+          }).then((ref) => {
+            request(`https://www.ptt.cc${url}`, function (error, response, body) {
+              var _begin = 0;
+              var _end = 0;
+              while (true) {
+                _begin = body.indexOf('nofollow', _end);
+                _end = body.indexOf('</a>', _begin);
+                if (_begin == -1)
+                  break;
+                ref.collection('images').add({
+                  url: body.substring(_begin + 10, _end)
+                });
+              }
+            });
+          });
+        } else {
+          snap.forEach(doc => {
+            BeautyRef.doc(doc.id).update({
+              nrec: nrec
+            })
+          })
+        }
+      });
+    }
+  });
 }
